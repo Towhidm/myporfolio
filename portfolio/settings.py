24b64767ee5202dedf2bfb089c534afce1774758
First-Project <towhidm644@gmail.com>
2024-09-05 15:40:42 +0600

    first commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6jhaa-+tsxh11j*o*49ijlvj3y)z&&#tkpgxi5vpvf&__j^gxl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join( BASE_DIR / "static")]
+STATICFILES_DIRS = os.path.join( BASE_DIR / "static"),
+STATIC_ROOT =  os.path.join( BASE_DIR / "staticfiles_build", "static")
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "media"
